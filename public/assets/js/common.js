3ad828b2a99242aac294db72c942767d7f4216cd
聂良鑫 <932861685@qq.com>
2020-03-15 10:06:41 +0800

    完成退出功能

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,10 +1,12 @@
-$('#logout').on('click',function(){
+//实现退出功能
+$('#logout').on('click', function () {
     let isConfirm=confirm('您确定要退出吗?');
     if(isConfirm){
       $.ajax({
         type:'post',
         url:'/logout',
-        success:function(){
+        success: function () {
+          //跳转页面
           location.href='login.html'
         },
         error:function(){

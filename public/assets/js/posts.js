136dc4c65ba8c20bb1cfb92b4448978be4d64996
聂良鑫 <932861685@qq.com>
2020-03-18 09:40:22 +0800

    完成分页功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -9,5 +9,24 @@ $.ajax({
         // console.log(res); 
         let html = template('pTpl', { data: res.records });
         $('tbody').html(html);
+        let pageHtml = template('pageTpl', res);
+        $('.pagination').html(pageHtml);
     }
 })
+function changePage(index) {
+    //点击页码a便签实现无刷新
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            page:index
+        },
+        success: function (res) {
+            // console.log(res); 
+            let html = template('pTpl', { data: res.records });
+            $('tbody').html(html);
+            let pageHtml = template('pageTpl', res);
+            $('.pagination').html(pageHtml);
+        }
+    })
+}
